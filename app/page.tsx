aa89af561b0109eb43a3e0ab5672ba3ab59a2be7
yong hyeok <yonghyeogi377@gmail.com>
2024-09-03 16:27:21 +0900

    init project and test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>우승을 위하여 아자아자아자~~!</div>
+      <div>우승을 위하여 아자아자아자~~! 할 수 있따 프론트엔드~~</div>
     </main>
   );
 }
