d86c117bea8096390e8385dc313f0a1f8e7eb60e
Yong Hyeok Lee <129850728+HelloWorldDogs223@users.noreply.github.com>
2024-09-09 23:15:39 +0900

    배포5

--- app/(pages)/kakao/page.tsx
+++ app/(pages)/kakao/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [code, setCode] = useState<any>("");
   useEffect(() => {
     const myCode = new URL(document.location.toString()).searchParams.get(
       "code"
     );
-    setCode(myCode); // 이상한코드가한가득
-  }, [code]);
+
+    const res = axios.post(
+      `https://todogochi.store/auth/sign-in/kakao`,
+      {
+        authCode: myCode,
+      },
+      { withCredentials: true }
+    );
+  }, []);
 
   return <div></div>;
 }
--- app/(pages)/signin/page.tsx
+++ app/(pages)/signin/page.tsx
@@ -2,18 +2,10 @@
 
 "use client";
 
-import axios from "axios";
-
 export default function Page() {
   const loginKakao = () => {
     const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
     window.location.href = link;
-
-    axios.post(
-      "https://todogochi.store/auth/refresh",
-      {},
-      { withCredentials: true }
-    );
   };
 
   return (
