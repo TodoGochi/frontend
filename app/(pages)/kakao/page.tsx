7aac5e2e253753375cca0932b3d7b1b7047ff862
Yong Hyeok Lee <129850728+HelloWorldDogs223@users.noreply.github.com>
2024-09-09 21:42:15 +0900

    배포3

--- app/(pages)/kakao/page.tsx
+++ app/(pages)/kakao/page.tsx
@@ -6,15 +6,18 @@ import { useEffect, useState } from "react";
 export default function Page() {
   const [code, setCode] = useState<any>("");
   useEffect(() => {
-    setCode(new URL(document.location.toString()).searchParams.get("code")); // 이상한코드가한가득
+    const myCode = new URL(document.location.toString()).searchParams.get(
+      "code"
+    );
+    setCode(myCode); // 이상한코드가한가득
     const res = axios.post(
       `https://todogochi.store/auth/sign-in/kakao`,
       {
-        authCode: code,
+        authCode: myCode,
       },
       { withCredentials: true }
     );
   }, [code]);
 
-  return <div>{code}</div>;
+  return <div></div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
       <div>TODO 고치</div>
       <div>
         <button onClick={() => router.push("/signin")}>로그인하기</button>
       </div>
-    </>
+    </div>
   );
 }
